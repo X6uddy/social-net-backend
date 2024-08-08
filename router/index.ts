8996c915fdb33398186e53789cb41a5cb9add05c
X6uddy <sashavolkov_03@mail.ru>
2024-08-08 17:23:13 +0400

    adding router

---
+++ router/index.ts
@@ -0,0 +1,11 @@
+import Router from 'express';
+// const {body} = require('express-validator');
+
+const router = Router.Router();
+
+// router.post('/registration', 
+//             body('email').isEmail(),
+//             body('password').isLength({min: 3, max: 20}),
+//             UserController.registration);
+
+export default router;
