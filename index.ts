f425ad65ce569c0be16dd00492a92c36988c0070
X6uddy <sashavolkov_03@mail.ru>
2024-08-08 17:23:04 +0400

    adding cors settings

--- index.ts
+++ index.ts
@@ -1,10 +1,16 @@
 import * as dotenv from 'dotenv';
 import express from "express";
 import cors from 'cors';
+import * as router from "./router";
 dotenv.config()
 
 const app = express();
-app.use(cors);
+app.use(cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL,
+}));
+app.use(express.json());
+app.use('/api', router);
 
 app.listen(process.env.PORT || 5000, () => {
     console.log(`Server started on ${process.env.PORT}`);
