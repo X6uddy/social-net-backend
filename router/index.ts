64646676f1a127275799785381f558b020dff717
X6uddy <sashavolkov_03@mail.ru>
2024-08-14 14:44:25 +0400

    adding basic routes

--- router/index.ts
+++ router/index.ts
@@ -1,13 +1,18 @@
-const Router = require('express');
+const Router = require('express').Router;
+const {body} = require('express-validator');
+const UserController = require('../controllers/user-controller');
+const authMiddleware = require('../middlewares/auth-middleware');
+
 const router = new Router();
-// import Router from 'express';
-// const {body} = require('express-validator');
 
-// const router = Router.Router();
+router.post('/registration', 
+            body('email').isEmail(),
+            body('password').isLength({min: 3, max: 20}),
+            UserController.registration);
+router.post('/login', UserController.login);
+// router.post('/logout', UserController.logout);
+// router.get('/activate/:link', UserController.activate);
+// router.get('/refresh', UserController.refresh);
+// router.get('/users', authMiddleware, UserController.users);
 
-// router.post('/registration', 
-//             body('email').isEmail(),
-//             body('password').isLength({min: 3, max: 20}),
-//             UserController.registration);
-
-export default router;
+module.exports = router;
