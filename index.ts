97d4302384df1d7f9ea1742fe1d14f97875ba3af
X6uddy <sashavolkov_03@mail.ru>
2024-08-14 14:43:36 +0400

    adding mongo database

--- index.ts
+++ index.ts
@@ -1,17 +1,41 @@
-import * as dotenv from 'dotenv';
-import express from "express";
-import cors from 'cors';
-import router from './router';
-dotenv.config()
+require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+const routers = require('./router/index');
+const errorMiddleware = require('./middlewares/error-middleware');
 
+const PORT = process.env.PORT || 5000;
+const URL_LINK = process.env.DB_URL;
 const app = express();
+
+// app.use(express.json());
+// app.use((req, res, next) => {
+//     res.setHeader("Access-Control-Allow-Origin", '*');
+//     res.header(
+//       "Access-Control-Allow-Headers",
+//       "Origin, X-Requested-With, Content-Type, Accept"
+//     );
+//     next();
+//   });
 app.use(cors({
     credentials: true,
     origin: process.env.CLIENT_URL,
 }));
 app.use(express.json());
-app.use('/api', router);
+app.use(cookieParser());
+app.use('/api', routers);
+app.use(errorMiddleware)
 
-app.listen(process.env.PORT || 5000, () => {
-    console.log(`Server started on ${process.env.PORT}`);
-});
+const start = async () => {
+    try {
+        await mongoose.connect(URL)
+        .then(() => console.log('Connected to DB'))
+
+        app.listen(PORT, () => console.log(`Server started on ${PORT} port`));
+    } catch (e) {
+        console.log(e);
+    }
+};
+start();
